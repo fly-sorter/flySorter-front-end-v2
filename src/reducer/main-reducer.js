e6343f9d2f17a05e014077a0efaa31a3976a0650
David Chambers <dlchambersjr@gmail.com>
2018-12-19 07:33:35 -0800

    remove old files

--- src/reducer/main-reducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import token from './token-reducer';
-import users from './users-reducer';
-import subAssy from './sub-assy-reducer';
-import parts from './parts-reducer';
-
-export default combineReducers({ token, users, subAssy, parts });
