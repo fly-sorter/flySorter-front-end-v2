b1aad18752c65a2d1d8f76f5ea440db0de38f587
David Chambers <dlchambersjr@gmail.com>
2018-12-21 12:36:05 -0800

    added auth wrapper

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+
+// Add AWS Resources
+import Amplify from 'aws-amplify';
+import aws_exports from '../aws-exports';
+import { withAuthenticator } from 'aws-amplify-react';
+
 import './reset.scss';
 import './base.scss';
 import AuthRedirect from './AuthRedirect/AuthRedirect.js';
@@ -9,8 +15,10 @@ import Dashboard from './Dashboard/Dashboard.js';
 import CreatePart from './CreatePart/CreatePart.js';
 import CreateSub from './CreateSub/CreateSub.js';
 import EditPart from './EditPart/EditPart.js';
-import CreateRedirect from './CreateRedirect/CreateRedirect.js';
+// import CreateRedirect from './CreateRedirect/CreateRedirect.js';
 import SubTableDash from './SubDashboard/SubDashboard.js';
+
+Amplify.configure(aws_exports);
 
 class App extends Component {
   render() {
@@ -23,7 +31,7 @@ class App extends Component {
             <Route path="/signup" component={Signup} />
             <Route path="/part-table" component={Dashboard} />
             <Route path="/sub-table" component={SubTableDash} />
-            <Route path="/create" component={CreateRedirect} />
+            {/* <Route path="/create" component={CreateRedirect} /> */}
             <Route path="/createpart" component={CreatePart} />
             <Route path="/editpart" component={EditPart} />
             <Route path="/createsub" component={CreateSub} />
@@ -34,4 +42,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withAuthenticator(App, true);
