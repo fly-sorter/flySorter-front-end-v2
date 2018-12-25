2fdc08562e080e028a7237a072f26da7584f71a8
David Chambers <dlchambersjr@gmail.com>
2018-12-24 16:43:27 -0800

    auth wrapper for presentation

--- src/components/AuthRedirect/AuthRedirect.js
+++ src/components/AuthRedirect/AuthRedirect.js
@@ -5,11 +5,12 @@ import cookies from 'react-cookies';
 class AuthRedirect extends React.Component {
   render() {
     //cookies.save('auth', 'I am a cookie');
-    const token = cookies.load('auth');
+    // const token = cookies.load('auth');
 
     return (
       <div>
-        {!token ? <Redirect to="/signin" /> : <Redirect to="/part-table" />}
+        {/* {!token ? <Redirect to="/signin" /> : <Redirect to="/part-table" />} */}
+        {<Redirect to="/part-table" />}
       </div>
     );
   }
